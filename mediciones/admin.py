afa0936112e76add78ddb6bb373333a04936959c
Fco. Armando Payan Guerrero <fapayan67@gmail.com>
2023-08-03 14:19:53 -0600

    aplicacion para mediciones

--- mediciones/admin.py
+++ mediciones/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from mediciones.models import Area,Region,Measurements
 
 # Register your models here.
+@admin.register(Area)
+class AreaAdmin(admin.ModelAdmin):
+    list_display = ['id','name','owner','creat_at']
+
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ['id','area','name','created_at']
+
+@admin.register(Measurements)
+class MeasurementsAdmin(admin.ModelAdmin):
+    list_display = ['id','region','temperagit ture','humidity','created_at']
--- mediciones/models.py
+++ mediciones/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+    owner = models.CharField(max_length=200)
+    phone = models.CharField(max_length=12)
+    mail = models.EmailField(null=True, blank=True)
+    country = models.CharField(max_length=100,null=True, blank=True)
+    state = models.CharField(max_length=100,null=True, blank=True)
+    city = models.CharField(max_length=100,null=True, blank=True)
+    postal_code = models.CharField(max_length=10,null=True, blank=True)
+    creat_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+class Region(models.Model):
+    area = models.ForeignKey(Area, related_name='region_area',on_delete=models.CASCADE)
+    name = models.CharField(max_length=100)
+    latitude = models.FloatField(max_length=0.0)
+    longitude = models.FloatField(max_length=0.0)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+class Measurements(models.Model):
+    region = models.ForeignKey(Region, related_name='region',on_delete=models.CASCADE)
+    temperature = models.FloatField(default=0)
+    humidity = models.FloatField(default=0)
+    created_at = models.DateTimeField(default=timezone.now)
+    note = models.TextField(null=True,blank=True)
